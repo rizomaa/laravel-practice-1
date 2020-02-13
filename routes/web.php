a74b3611cf05e99f29f702bd3d483e90b2babc9d
Ryzomych <rizoma@mail.riri>
2020-02-13 23:02:56 +0300

    Display all question

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('questions', 'QuestionsController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
