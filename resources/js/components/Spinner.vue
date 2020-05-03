6a2d7f8d697cbda7e8c622c7e0fe48b3684ed0a8
Ryzomych <rizoma@mail.riri>
2020-05-03 21:11:28 +0300

    added spinner component

---
+++ resources/js/components/Spinner.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="spinner">
+        <div class="fa fa-spinner fa-spin fa-3x"></div>
+    </div>
+</template>
+<style scoped>
+.spinner {
+    color: #ddd; 
+    text-align: center;
+}
+</style>
+
+<script>
+export default {
+    
+}
+</script>
